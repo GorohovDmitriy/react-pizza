5d0f768aac910b6f040a3bb3e52eb7d76b434f86
GorohovDmitriy <dmitriy2893@gmail.com>
2021-05-11 16:10:50 +0300

    Redux Hooks, useSelector, useDispatch,  подключил фэйковый сервет json на 3001 порту

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { connect } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 
 import { Route } from 'react-router';
@@ -9,55 +9,23 @@ import { Home, Cart } from './pages';
 import { setPizzas } from './redux/actions/pizzas';
 
 
+function App() {
+	const dispatch = useDispatch();
 
-
-
-// function App() {
-
-
-
-// React.useEffect(() => {
-// 	axios.get('http://localhost:3000/db.json').then(({ data }) => {
-// 		setPizzas(data.pizzas);
-// 	});
-// }, []);
-
-
-// 	return
-// }
-
-class App extends React.Component {
-
-	componentDidMount() {
-		axios.get('http://localhost:3000/db.json').then(({ data }) => {
-			this.props.setPizzas(data.pizzas);
+	React.useEffect(() => {
+		axios.get('http://localhost:3001/pizzas').then(({ data }) => {
+			dispatch(setPizzas(data));
 		});
-	}
+	}, []);
 
-	render() {
-		return (
-			<div className="wrapper">
-				<Header />
-				<div className="content">
-					<Route path="/" render={() => <Home items={this.props.items} />} exact />
-					<Route path="/cart" component={Cart} exact />
-				</div>
-			</div>
-		);
-	}
+	return (<div className="wrapper">
+		<Header />
+		<div className="content">
+			<Route path="/" component={Home} exact />
+			<Route path="/cart" component={Cart} exact />
+		</div>
+	</div>);
 }
 
-const mapStateToProps = state => {
-	return {
-		items: state.pizzas.items,
-		filters: state.tilters,
-	}
-}
+export default App;
 
-const mapDispatchToProps = dispatch => {
-	return {
-		setPizzas: (items) => dispatch(setPizzas(items))
-	};
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
