79be98565628cf17545a27929e0bae14f7892c61
GorohovDmitriy <dmitriy2893@gmail.com>
2021-05-14 19:33:46 +0300

    Доделал корзину, закончил проект!!!

--- src/redux/actions/cart.js
+++ src/redux/actions/cart.js
@@ -2,3 +2,21 @@ export const addPizzaToCart = (pizzaobj) => ({
 	type: 'ADD_PIZZA_CART',
 	payload: pizzaobj,
 })
+
+export const clearCart = () => ({
+	type: 'CLEAR_CART',
+})
+
+
+export const removeCartItem = id => ({
+	type: 'REMOVE_CART_ITEM',
+	payload: id,
+})
+export const plusCartItem = id => ({
+	type: 'PLUS_ITEM',
+	payload: id,
+})
+export const minusCartItem = id => ({
+	type: 'MINUS_ITEM',
+	payload: id,
+})
